c794a686eb9cc75c127abdc1cfb2e215e431fa97
priyasathya <125353483+JPriyasathya@users.noreply.github.com>
2024-10-26 09:41:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
+import os
 from flask import Flask, request, jsonify
 import joblib
 
-# Load the model and vectorizer
+# Load model and vectorizer
 model = joblib.load('random_forest_model.pkl')
 vectorizer = joblib.load('vectorizer.pkl')
 
@@ -19,4 +20,5 @@ def predict():
     return jsonify({'prediction': prediction})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
